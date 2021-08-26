a773ac3e5de2cf525bceb93e3aafeacf35453e31
replaceits <sid@sidw.me>
2021-08-26 15:32:34 -0400

    Refactor data

--- src/data/services.js
+++ src/data/services.js
@@ -13,28 +13,20 @@ const makeNewService = (...args) => (
 );
 
 class Services {
-  constructor(...services) {
-    this.services = [];
-
-    for (const service of services) {
-      const [name, image] = service;
-
-      this.services.push(makeNewService(name, image));
-    }
-
-    this.services = Object.freeze(this.services);
-  }
+  services = Object.freeze(Services.services.map(([...service]) => makeNewService(...service)))
 
   top(num, page = 0) {
     return this.services.slice(page * num, (page * num) + num);
   }
+
+  static services = [
+    ['Aeration', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/AerationAfter.jpg'],
+    ['Pruning', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/PruningAfter.jpg'],
+    ['Shrubbery', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/ShrubberyCareAfter.jpg'],
+    ['Weeding', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/WeedingAfter.jpg'],
+    ['Mowing', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/Mowing.jpg'],
+    ['Mulching', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/Mulching1.jpg'],
+  ]
 }
 
-export default new Services(
-  ['Aeration', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/AerationAfter.jpg'],
-  ['Pruning', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/PruningAfter.jpg'],
-  ['Shrubbery', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/ShrubberyCareAfter.jpg'],
-  ['Weeding', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/WeedingAfter.jpg'],
-  ['Mowing', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/Mowing.jpg'],
-  ['Mulching', 'https://cleanlineslawncare.s3.us-east-1.amazonaws.com/oldimages/Mulching1.jpg'],
-);
+export default new Services();
