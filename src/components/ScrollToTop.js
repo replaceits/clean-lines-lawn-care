f419b76a1bfb0c27720f3048ee8b3d799fa7432d
replaceits <sid@sidw.me>
2021-08-26 14:19:40 -0400

    Add Redirects to reviews

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { withRouter } from 'react-router';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { ScrollProvider } from '../contexts/scroll';
